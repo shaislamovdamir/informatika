2f4dc9927b16f74b39ffc3b4a8f2f71424917010
shaislamovdamir <144202062+shaislamovdamir@users.noreply.github.com>
2023-09-27 11:15:49 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,7 @@
+x = float(input())
+y = float(input())
+z = float(input())
 import math
-
-def task1():
-  pass
+a = math.sin((math.fabs(x-1))**1/3) - math.cos((math.fabs(y))**1/4) / 1 + x**1/2 + y**1/5
+b = (math.fabs(x - 1))**1/2 + math.tan(y) + z**2 / 3 - (math.log(1 + y))
+print (a,b)
