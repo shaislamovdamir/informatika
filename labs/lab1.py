eb17bceb3bfbe04974e9f942a90fb24d4572fde7
shaislamovdamir <144202062+shaislamovdamir@users.noreply.github.com>
2023-10-25 11:03:28 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -7,6 +7,14 @@ a = math.sin((math.fabs(x-1))**1/3) - math.cos((math.fabs(y))**1/4) / 1 + x**1/2
 b = (math.fabs(x - 1))**1/2 + math.tan(y) + z**2 / 3 - (math.log(1 + y))
 print (a,b)
 
+zadanie2
+import math
+def f(x):
+    a = -2
+    return (math.sqrt(x)) / (math.sqrt(x + a)) + 2/3 * x
+x = float(input())
+print(f(x))
+
 zadanie3
 import math
 def f(x):
@@ -14,3 +22,11 @@ def f(x):
 x = float(input())
 print(f(x))
 
+zadanie4
+h = float(input())
+l = float(input())
+k = float(input())
+import math
+r = math.sqrt(l**2 - h**2)
+rd = r * k
+print(r,rd)
